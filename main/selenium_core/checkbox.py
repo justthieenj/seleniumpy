90c774dfb87663ffedd8af47ecf08e88204e35a8
Thien Nguyen <thien.nguyenkh@saigontechnology.com>
2021-12-06 16:09:46 +0700

    :baby: impl checkbox element

---
+++ main/selenium_core/checkbox.py
@@ -0,0 +1,11 @@
+from main.selenium_core.base_control import BaseControl
+
+
+class CheckBox(BaseControl):
+
+    def __init__(self, locator):
+        self._locator = locator
+        super().__init__(locator)
+
+    def is_checked(self):
+        return self.get_element().is_selected()
